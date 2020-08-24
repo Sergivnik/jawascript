cef24e3df0b99b9d4add6830f508355b0949c86d
Serg <sergivnik@mail.ru>
2020-08-24 20:51:14 +0300

    task-3

--- ДЗ-5/task-1/js/chessdesk.js
+++ ДЗ-5/task-1/js/chessdesk.js
@@ -1,6 +1,30 @@
 var cages = [];
 var desk = document.querySelector("div.desk");
 var litter = [" ", "A", "B", "C", "D", "E", "F", "G", "H", " "];
+var chessMenBlack = [
+  "",
+  "&#9820",
+  "&#9822",
+  "&#9821",
+  "&#9819",
+  "&#9818",
+  "&#9821",
+  "&#9822",
+  "&#9820",
+  "",
+];
+var chessMenWhite = [
+  "",
+  "&#9814",
+  "&#9816",
+  "&#9815",
+  "&#9813",
+  "&#9812",
+  "&#9815",
+  "&#9816",
+  "&#9814",
+  "",
+];
 for (var j = 0; j < 10; j++) {
   for (var i = 0; i < 10; i++) {
     cages[(i, j)] = document.createElement("div");
@@ -21,7 +45,20 @@ for (var j = 0; j < 10; j++) {
         cages[(i, j)].classList.add("roteteCage");
       }
       continue;
-      continue;
+    }
+    if (j == 1) {
+      cages[(i, j)].insertAdjacentHTML("afterbegin", chessMenBlack[i]);
+      //cages[(i, j)].classList.add("roteteCage");
+    }
+    if (j == 2) {
+      cages[(i, j)].insertAdjacentHTML("afterbegin", "&#9823");
+      //cages[(i, j)].classList.add("roteteCage");
+    }
+    if (j == 7) {
+      cages[(i, j)].insertAdjacentHTML("afterbegin", "&#9817");
+    }
+    if (j == 8) {
+      cages[(i, j)].insertAdjacentHTML("afterbegin", chessMenWhite[i]);
     }
     if (j % 2) {
       if (i % 2) {
